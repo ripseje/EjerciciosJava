5f8e56fa60c354732c1dc9f315b095940c729a94
Sergio <Sergio@DESKTOP-M1VFANA>
2019-11-28 17:42:54 +0100

    Con el segundo ejercicio terminado

--- src/ejerciciosbasicos/EjerciciosBasicos.java
+++ src/ejerciciosbasicos/EjerciciosBasicos.java
@@ -24,7 +24,17 @@ public class EjerciciosBasicos {
     }
     
     public int multa (int velocidad, boolean birthday){
-       return 0;
+       if(birthday){ //si es mi cumple el guardia me quita 5 km/h de velocidad
+           velocidad -= 5;
+       }
+       if(velocidad <= 60){
+           return 0;
+       }
+       if(velocidad > 60 &&  velocidad <= 80){
+           return 1;
+       }
+       
+       return 2; //el resto de casos que son más de 80
     }
     
     /**
@@ -32,9 +42,18 @@ public class EjerciciosBasicos {
      */
     public static void main(String[] args) {
         EjerciciosBasicos ejercicios = new EjerciciosBasicos();
+        //test ejercicio 1
         System.out.println(ejercicios.fiestaArdilla(30, false));
         System.out.println(ejercicios.fiestaArdilla(50, false));
         System.out.println(ejercicios.fiestaArdilla(70, true));
+        //test ejercicio 2
+        System.out.println(ejercicios.multa(60, false));
+        System.out.println(ejercicios.multa(65, false));
+        System.out.println(ejercicios.multa(65, true));
+        System.out.println(ejercicios.multa(80, false));
+        System.out.println(ejercicios.multa(80, true));
+        System.out.println(ejercicios.multa(85, true));
+        System.out.println(ejercicios.multa(85, false));
     }
     
 }
