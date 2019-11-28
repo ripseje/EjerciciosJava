124b8f363ab849700dd21346ea9815c60d6da7e3
Sergio <Sergio@DESKTOP-M1VFANA>
2019-11-28 16:53:48 +0100

    Versión inicial con el rpimer ejercicio hecho

---
+++ src/ejerciciosbasicos/EjerciciosBasicos.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosbasicos;
+
+/**
+ *
+ * @author Sergio
+ */
+public class EjerciciosBasicos {
+    
+    public boolean fiestaArdilla (int numBellotas, boolean finDeSemana){
+        if(finDeSemana){
+            return true;
+        }
+        else{
+            if(numBellotas >= 40 && numBellotas <= 60){
+            return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EjerciciosBasicos ejercicios = new EjerciciosBasicos();
+        System.out.println(ejercicios.fiestaArdilla(30, false));
+        System.out.println(ejercicios.fiestaArdilla(50, false));
+        System.out.println(ejercicios.fiestaArdilla(70, true));
+    }
+    
+}
